b7061c8baaed4ab817fa98476262c056e0cd08d8
AlexanderBotnari <63365662+AlexanderBotnari@users.noreply.github.com>
2020-05-18 05:21:23 -0700

    Update Aplication.java

--- Aplication.java
+++ Aplication.java
@@ -7,30 +7,30 @@ public class Aplication {
 
 	public static void main(String[] args) throws IOException {
 		
-     	String pizza = "Pizza 75.00 2\n";
+     	        String pizza = "Pizza 75.00 2\n";
 		String kebab = "Kebab 45.00 1\n";
 		String cola  = "Cola   9.50 3";
 		
 		double total_price = 75.00 + 45.00 + 9.50;
 		
 	    FileWriter order = new FileWriter("orders.txt");
-    	order.write(pizza + kebab + cola );
-		order.close();
+    	    order.write(pizza + kebab + cola );
+	    order.close();
 	
-		File file = new File("orders.txt");
-		String food;
+	    File file = new File("orders.txt");
+	    String food;
 		if(file.exists()) {
 			System.out.println("File Found !!!");
 			System.out.println("--------------");
 			Scanner scanner = new Scanner( file );
 			for(int i = 1 ; i <= 3 ; i++) {
 			    food = scanner.nextLine();
-				System.out.println(food);
+			    System.out.println(food);
 			}
 			System.out.println("--------------------");
 			System.out.println("Total price : "+total_price);
 		}else {
 			System.err.println("File Not Found! ");
 		}
-    }
+       }
 }
